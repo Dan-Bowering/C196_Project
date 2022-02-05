139a6775c7c94786a1be85c82de69e66a434792d
DanBowering <danbowering@gmail.com>
2022-02-04 21:33:59 -0500

    Sending data to the Term Details section when a term is selected.

--- app/src/main/java/com/example/c196project/UI/CourseList.java
+++ app/src/main/java/com/example/c196project/UI/CourseList.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.example.c196project.Database.Repository;
 import com.example.c196project.Entity.Course;
@@ -18,13 +19,31 @@ import java.util.List;
 
 public class CourseList extends AppCompatActivity {
 
-    int termID;
+    EditText editId;
+    EditText editName;
+    EditText editStart;
+    EditText editEnd;
+    Integer id;
+    String name;
+    String start;
+    String end;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_list);
-        termID = getIntent().getIntExtra("id", -1);
+        editId = findViewById(R.id.editTermId);
+        editName = findViewById(R.id.editTermName);
+        editStart = findViewById(R.id.editTermStart);
+        editEnd = findViewById(R.id.editTermEnd);
+        id = getIntent().getIntExtra("id", -1);
+        name = getIntent().getStringExtra("name");
+        start = getIntent().getStringExtra("start");
+        end = getIntent().getStringExtra("end");
+        editId.setText(Integer.toString(id));
+        editName.setText(name);
+        editStart.setText(start);
+        editEnd.setText(end);
         RecyclerView recyclerView = findViewById(R.id.recyclerView2);
         Repository repository = new Repository((getApplication()));
         final CourseAdapter adapter = new CourseAdapter(this);
@@ -32,7 +51,7 @@ public class CourseList extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         List<Course> associatedCourses = new ArrayList<>();
         for (Course c : repository.getAllCourses()) {
-            if (c.getTermId() == termID)
+            if (c.getTermId() == id)
                 associatedCourses.add(c);
         }
         adapter.setCourses(associatedCourses);
